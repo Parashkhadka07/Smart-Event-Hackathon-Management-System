94f7b953f395906601a4ab0e0fec5701fa8db757
Parshkhadka07 <parashkhadka020@gmail.com>
2026-08-03 18:40:33 +0545

    token model is created

--- Backend/user/admin.py
+++ Backend/user/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User ,token
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ['id', 'username', 'email',  'role']
+    list_display = ['id', 'username', 'email','password',  'role']
     search_fields = ['username', 'email']
 
 admin.site.register(User, CustomUserAdmin)
+
+class Otp_admin(admin.ModelAdmin):
+    model=token
+    list_display=['user','OTP']
+    search_fields=['username']
+
+admin.site.register(token,Otp_admin)
--- Backend/user/models.py
+++ Backend/user/models.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 # Create your models here.
 class User(AbstractUser):
-    
+   
+    first_name = None
+    last_name = None
+    fullname=models.CharField(max_length=100,null=True)
     ROLE_CHOICES={
         "P":"participant",
         "J":"judge",
@@ -11,3 +14,7 @@ class User(AbstractUser):
     }
     id=models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
     role=models.CharField(choices=ROLE_CHOICES,max_length=12 )
+
+class token(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    OTP=models.TextField(unique=True)
