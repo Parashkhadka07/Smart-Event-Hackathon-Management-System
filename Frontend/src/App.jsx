2f464e494bfeb043348358b4e21be5fa19436755
Parshkhadka07 <parashkhadka020@gmail.com>
2026-08-14 22:32:27 +0545

    yo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,11 +4,16 @@ import Navbar from "./components/html/navbar";
 import Home from "./pages/html/home";
 import { Footer, SecondFooter } from "./components/html/footer";
 function App() {
+  const [num,setNum]=useState(5)
+  
+  
   return (
     <>
      <h1><Navbar /> </h1> 
       <h1><Home /> </h1>
-      
+      <button onClick={()=>{setNum(num-1)}}>decrease</button>
+      <h1>The number is : {num}</h1>
+      <button onClick={()=>{setNum(num+1)}}>increase</button>
       <Footer />
       <SecondFooter />
     </>
