8f1a20de6a693381a772ef11e728fa5ef5762f9c
Parshkhadka07 <parashkhadka020@gmail.com>
2026-08-19 23:37:16 +0545

    home page

--- Frontend/src/components/html/Button.jsx
+++ Frontend/src/components/html/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/button.css";
 const Button = (props) => {
-  return <a className="button" href="#">{props.name}</a>;
+  return <a className="button" href="#"  style={{ height: props.height, width: props.width }}>{props.name}</a>;
 };
 
 export default Button;
--- Frontend/src/components/html/Home.jsx
+++ Frontend/src/components/html/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/home.css";
+import Button from "../html/Button.jsx";
 const Home = () => {
   return (
     <>
@@ -14,12 +15,112 @@ const Home = () => {
 
           <div className="big_text">
             <p className="textfs"> Turn Ideas Into</p>
-            <p className="cloured_text">Innovation</p> <p className="text">& Breakthroughs</p>
+            <p className="cloured_text">Innovation</p>{" "}
+            <p className="text">& Breakthroughs</p>
           </div>
-          <div></div>
-          <div></div>
+          <div className="hero_text">
+            NerdHub empowers hackathon organizers, mentors, and developers
+            <br /> to collaborate, submit verified code, and compete seamlessly
+            with live <br />
+            automated leaderboards.
+          </div>
+          <div className="hero_button">
+            <Button name="Explore Events" height="50px" width="150px" />
+            <Button name="Host an Event" height="50px" width="150px" />
+          </div>
+          <hr />
+          <div className="public_proof">
+            <div>
+              <img
+                className="images_hero"
+                src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&fit=crop&w=100&q=80"
+                alt="User"
+              />
+              <img
+                className="images_hero"
+                src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&w=100&q=80"
+                alt="User"
+              />
+              <img
+                className="images_hero"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=100&q=80"
+                alt="User"
+              />
+            </div>
+            <span>
+              Joined by <strong>2,500+ developers</strong> across top
+              universities & tech companies.
+            </span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="BOX">
+            <div className="circle">
+              <div className="red_circle"></div>
+              <div className="yellow_circle"></div>
+              <div className="green_circle"></div>
+            </div>
+            <div className="box_top">
+              <div>nerdhub-live-hackathon.ts</div>
+              <div className="live_text">LIVE</div>
+            </div>
+          </div>
+          <hr style={{ margin: "0 30px 0 20px" }} />
+
+          <div className="status">
+            <div>// Active Hackathon: CodeSprint Global 2026</div>
+            <div>
+              <span className="status_box">Status:</span>
+              <span className="jug"> JUDGING_IN_PROGRESS</span>
+            </div>
+            <div>
+              Submissions: <span className="projects"> 142 Projects</span>
+            </div>
+          </div>
+          <div className="sanskar_div">
+            <div className="progess_status">
+              <div className="progress_text">
+                <strong>Current Top Team: Team NeuralCraft</strong>
+              </div>
+              <div className="progress_point">98.5 pts</div>
+            </div>
+            <div className="progress">
+              <div className="progressfill"></div>
+            </div>
+          </div>
+          <div className="basnet_don">
+            <div>
+              <div>
+                <strong>Prize Pool</strong>
+              </div>
+              <div>$15,000 + Cloud Credits</div>
+            </div>
+            <div>
+              <div>
+                <Button name="View Event" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="total_events">
+        <div className="data">
+          <div  className="number hosted">120+</div>
+          <div className="number_text">Events Hosted</div>
+        </div>
+        <div className="data">
+          <div className="number active">2,500+</div>
+          <div className="number_text">Active Participants</div>
+        </div>
+        <div className="data">
+          <div className="number teams">600+</div>
+          <div className="number_text">Teams Formed</div>
+        </div>
+        <div className="data">
+          <div className="number project" >1,200+</div>
+          
+          <div className="number_text">Projects Built</div>
+        </div>
       </div>
     </>
   );
