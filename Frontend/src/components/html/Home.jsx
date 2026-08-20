75f6e03fae604a0753d559bf9261cba38f5b71a4
Parshkhadka07 <parashkhadka020@gmail.com>
2026-08-20 14:28:44 +0545

    home page completed

--- Frontend/src/components/html/Home.jsx
+++ Frontend/src/components/html/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../css/home.css";
 import Button from "../html/Button.jsx";
-import photo1 from "../../assets/images/photo1.avif"
-import photo2 from "../../assets/images/photo2.avif"
-import photo3 from "../../assets/images/photo3.avif"
+import photo1 from "../../assets/images/photo1.avif";
+import photo2 from "../../assets/images/photo2.avif";
+import photo3 from "../../assets/images/photo3.avif";
 const Home = () => {
   return (
     <>
@@ -34,21 +34,9 @@ const Home = () => {
           <hr />
           <div className="public_proof">
             <div>
-              <img
-                className="images_hero"
-                src={photo1}
-                alt="User"
-              />
-              <img
-                className="images_hero"
-                src={photo2}
-                alt="User"
-              />
-              <img
-                className="images_hero"
-                src={photo3}
-                alt="User"
-              />
+              <img className="images_hero" src={photo1} alt="User" />
+              <img className="images_hero" src={photo2} alt="User" />
+              <img className="images_hero" src={photo3} alt="User" />
             </div>
             <span>
               Joined by <strong>2,500+ developers</strong> across top
@@ -108,7 +96,7 @@ const Home = () => {
       </div>
       <div className="total_events">
         <div className="data">
-          <div  className="number hosted">120+</div>
+          <div className="number hosted">120+</div>
           <div className="number_text">Events Hosted</div>
         </div>
         <div className="data">
@@ -120,9 +108,62 @@ const Home = () => {
           <div className="number_text">Teams Formed</div>
         </div>
         <div className="data">
-          <div className="number project" >1,200+</div>
-          
+          <div className="number project">1,200+</div>
+
           <div className="number_text">Projects Built</div>
+        </div>
+      </div>
+      <div className="home_bottom">
+        <div>
+          <div className="bottom_big_text">How NerdHub Works</div>
+          <div className="bottom_small_text">
+            <strong>
+              A seamless journey from initial idea generation to real-time
+              evaluation and leaderboard recognition.
+            </strong>
+          </div>
+        </div>
+        <div className="box_container">
+          <div className="boxes">
+            <div className="bignumber">1</div>
+            <div className="bigtext_bottom">Discover</div>
+            <div className="small_text_bottom">
+              Browse online or in-person hackathons based on technologies,
+              dates, and prizes.
+            </div>
+          </div>
+          <div className="boxes">
+            <div className="bignumber">2</div>
+            <div className="bigtext_bottom">Register</div>
+            <div className="small_text_bottom">
+              Sign up solo or form/join a cross-functional team with automated
+              invite codes.
+            </div>
+          </div>
+          <div className="boxes">
+            <div className="bignumber">3</div>
+            <div className="bigtext_bottom">Build</div>
+            <div className="small_text_bottom">
+              Connect with assigned mentors, participate in office hours, and
+              write clean code.
+            </div>
+          </div>
+          <div className="boxes">
+            <div className="bignumber">4</div>
+            <div className="bigtext_bottom">Submit</div>
+            <div className="small_text_bottom">
+              Upload code repositories, live demo links, presentations, and
+              solution blueprints.
+            </div>
+          </div>
+          <div className="boxes">
+            <div className="bignumber">5</div>
+            <div className="bigtext_bottom">Compete</div>
+            <div className="small_text_bottom">
+              Get scored by judges on innovation, tech stack, and UI to win
+              prizes on live leaderboards.
+            </div>
+          </div>
         </div>
       </div>
     </>
